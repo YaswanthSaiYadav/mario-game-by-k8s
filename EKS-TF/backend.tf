4c138e9c52ba3f6e8da651e2d535e55a225a2142
Karankolhe12 <139026443+Karankolhe12@users.noreply.github.com>
2024-03-05 19:28:04 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "python-devops-batch-lokesh" # Replace with your actual S3 bucket name
+    bucket = "karankolhe1223" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
